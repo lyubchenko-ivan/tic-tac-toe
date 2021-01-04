c08c9278ff2a85b3e092bc6a405c80679aaff742
lyubchenko-ivan <lubchenko2044@gmail.com>
2021-01-04 20:36:54 +0300

    last version project

--- classes.rb
+++ classes.rb
@@ -1,43 +1,204 @@
 # класс описывающий игровую поверхность
 class Desk
     attr_reader :cols, :rows
+    attr_accessor :desk
+
     def initialize
         @cols = 3
         @rows = 3
-    end    
+        @desk =  [
+            ['-', '-', '-'],
+            ['-', '-', '-'],
+            ['-', '-', '-']
+        ]
+    end
+    
+    
+    def greeting_desk
+        self.rows.times do |row|
 
-    def display
-        system("clear")
-        self.rows.times do
-            
-            self.cols.times do
-                print"1 |"
+            self.cols.times do |column|
+                print "  #{row * 3 + column + 1}"
+                print "|" if column != 2
             end
             puts
-            # self.cols.times do
-            #     print"   "
-            # end
-            # puts
-
-            self.cols.times do
-                print "--+"
+            if (row != 2)
+                self.cols.times do |column|
+                    print "---"
+                    print '+' if (column != 2)
+                end
+                puts
             end
-            puts
+
         end
-    end    
+    end
+
+    def position_empty?(number)
+        row = (number - 1)  / 3
+        column = (number - 1) % 3
+        if (self.desk[row][column] == '-') then return  true
+        else return false
+        end
+    end
+
+    def display_desk
+        system("clear")
+        self.rows.times do |row|
+            self.cols.times do |column|
+                print " #{self.desk[row][column]} "
+                print "|" if column != 2
+            end
+            puts
+
+            if (row != 2)
+                self.cols.times do |column|
+                    print "---"
+                    print '+' if column != 2
+                end
+                puts
+            end
+
+
+        end
+    end
+
+    def take_sym(sym, index)
+        row = (index - 1) / 3
+        col = (index - 1) % 3
+        self.desk[row][col] = sym
+    end
+
+    def game_over?(player)
+        #три по горизонтали
+        self.desk.each  do |row|
+             if (row.all? {|el| el == player.amplua})
+                puts "Победил игрок #{player.username}. Игра окончена."
+                return  true
+            end
+        end
+
+        #три по вертикали
+        self.cols.times do |column|
+            status = true
+            self.rows.times do |row|
+                if (!(self.desk[row][column] == self.desk[0][column] && self.desk[row][column] == player.amplua) )
+                    status = false
+                end
+            end
+
+            if (status)
+                puts "Победил игрок #{player.username}. Игра окончена."
+                return true
+            end
+        end
+
+        #три по  главной диагонали
+        status = true
+        self.rows.times do |index|
+            if !(self.desk[index][index] == self.desk[0][0] && player.amplua == self.desk[0][0]) then status = false end
+
+        end
+        if status == true
+            puts "Победил игрок #{player.username}. Игра окончена."
+            return  true
+        end
+
+        #три по побочной диагонали
+        status = true
+        self.rows.times do |index|
+            if (self.desk[index][self.desk.length - 1 - index] != self.desk[0][self.desk.length - 1] || self.desk[0][self.desk.length - 1] != player.amplua)
+                status = false
+            end
+        end
+        if status == true
+            puts "Победил игрок #{player.username}. Игра окончена."
+            return  true
+        end
+
+        return  false
+
+
+    end
 end
+
+
 
 # клас описывающий игрока
 class Player
+    @@count_players = 1
+    @@broned = nil
+    attr_reader :username, :id, :amplua, :game_desk
 
-end    
+    def initialize(username = "player#{@@count_players}")
+        if (username == ' ') then username = "player#{@@count_players}" end
+        @username = username
+        @id = @@count_players
+        @@count_players += 1
+    end
 
-# класс описвающий процесс игры
-class Game 
+    def initialize_amplua(amplua)
+        if (self.id == 1)
+            if (amplua == 1)
+                @amplua = 'X'
+            else 
+                @amplua = 'O'
+            end    
+            @@bronned = @amplua
+        elsif(self.id == 2)
+            if (@@bronned == 'X')
+                @amplua = 'O'
+            else @amplua = 'X'
+            end    
+        end    
+    end
 
-end    
 
-# класс описывающий ячейку
-class Cell
 
-end    
+    def makeAMove(desk)
+        @game_desk = desk
+        index = nil
+        begin
+            puts "#{self.username}, в какую ячейку Вы хотите поставить \"#{self.amplua}\""
+            index = gets.chomp.to_i
+        end while(!(index >= 1 && index <= 9) || !(self.game_desk.position_empty?(index)))
+
+        self.game_desk.take_sym(self.amplua, index)
+    end
+end
+
+
+class Game
+    @@moves = 0
+    attr_reader :player1, :player2
+    def initialize(player1, player2)
+        @player1 = player1
+        @player2 = player2
+    end
+
+    def game_over?
+        return  true  if (@@moves >= 9)
+        return  false
+    end
+
+    def make_a_move(desk)
+        @game_desk = desk
+        @@moves += 1
+
+        if (@@moves % 2 == 0)
+            #make move a player2
+            self.player2.makeAMove(desk)
+        else
+            #make move a player1
+            self.player1.makeAMove(desk)
+        end
+
+    end
+
+    def game_over?
+        if (@@moves == 9)
+            system("clear")
+            puts("Ничья. Спасибо за игру")
+            return true
+        end
+    end
+end
--- game.rb
+++ game.rb
@@ -1,5 +1,53 @@
 require './classes.rb'
+system("clear")
 
-desk = Desk.new
+puts 'Привет, это игра крестики-нолики'
 
-desk.display
+# player1
+print 'Введите имя первого игрока '
+username = gets.chomp
+player1 = Player.new(username)
+
+
+# player2
+print 'Введите имя второго игрока '
+username = gets.chomp
+player2 = Player.new(username)
+
+game = Game.new(player1, player2)
+# оперделение кто играет ноликами, а кто крестиками
+system("clear")
+puts "Теперь определимся, кто чем будет играть..."
+puts "#{player1.username}, чем Вы хотите играть?"
+puts "  1.Крестики"
+puts "  2.Нолики"
+
+amplua = nil
+begin
+    print "Введите цифру: "
+    amplua = gets.chomp.to_i
+end while (amplua != 1 && amplua != 2)
+
+player1.initialize_amplua(amplua)
+player2.initialize_amplua(0)
+puts "OK!"
+
+system("clear")
+puts "Ознакомимся с игрорвым полем..."
+
+
+game_desk = Desk.new
+
+game_desk.greeting_desk
+puts "Вот по этим номерам и будем выставлять крестики и нолики"
+puts("Нажмите Enter")
+gets
+
+
+begin
+  game_desk.display_desk
+  puts("\n")
+  game.make_a_move(game_desk)
+end while (!game_desk.game_over?(player1) && !game_desk.game_over?(player2) && !game.game_over?)
+
+# game_desk.display_desk
